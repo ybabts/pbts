4554689922ab61427b6f773167d3f7a102d39be0
Tyler Mueller <muelltyl@gmail.com>
2023-05-13 14:52:27 -0500

    adds offset to lendel

--- src/wires/length-delimited/mod.ts
+++ src/wires/length-delimited/mod.ts
@@ -12,17 +12,17 @@ export function encode(data: Uint8Array): Uint8Array {
   return buffer;
 }
 
-export function decode(buffer: Uint8Array): Uint8Array {
+export function decode(buffer: Uint8Array, offset = 0): Uint8Array {
   if (buffer.length === 0) throw new RangeError("Buffer is empty");
   if (buffer.length === 1) {
     throw new RangeError("Buffer is too short to contain a message length");
   }
   // I assume that the length of the message will never be greater than MAX_SAFE_INTEGER
   // there's no way that you'll ever need a message that's 9 petabytes long
-  const length = varint.decodeNumber(buffer, 0);
+  const length = varint.decodeNumber(buffer, offset);
   const bufferLength = varint.calcSize(length);
   if (buffer.length < length + bufferLength) {
     throw new RangeError("Buffer is too short to contain the message");
   }
-  return buffer.subarray(bufferLength, bufferLength + length);
+  return buffer.subarray(bufferLength + offset, bufferLength + length + offset);
 }
